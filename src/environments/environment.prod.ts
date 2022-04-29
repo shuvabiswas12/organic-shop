a8501d80b999c1a44416d67778eeae850ad9222b
Shuva Biswas <shuvabiswas12@gmail.com>
2022-04-29 16:14:45 +0600

    first commit.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'organic-shop-angular',
+    appId: '1:863981859754:web:457347aedc0cffb2a4f1d3',
+    storageBucket: 'organic-shop-angular.appspot.com',
+    apiKey: 'AIzaSyCaU52vr4W76qQ8-7dicYQXSCX0VaCl2Vk',
+    authDomain: 'organic-shop-angular.firebaseapp.com',
+    messagingSenderId: '863981859754',
+  },
   production: true
 };
