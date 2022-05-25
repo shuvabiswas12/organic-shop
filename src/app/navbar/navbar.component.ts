93b167f45ce70ff075261d0ba49a51d65a40085d
Shuva Biswas <shuvabiswas12@gmail.com>
2022-05-25 12:46:40 +0600

    fixed login button shwoing issue after loading each time.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,17 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   appUser: AppUser;
+  shouldShowLogin: boolean;
+
   constructor(public authService: AuthService) {
     this.appUser = {} as AppUser;
+    this.shouldShowLogin = true;
   }
 
   ngOnInit(): void {
     this.authService.appUser$.subscribe({
-      next: (user) => (this.appUser = user as AppUser),
+      next: (user) => {
+        this.appUser = user as AppUser;
+      },
     });
+
+    this.shouldShowLogin = !this.authService.isTokenAvailable();
   }
 
   logout() {
     this.authService.logout();
+    this.shouldShowLogin = !this.authService.isTokenAvailable();
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs/internal/Observable';
 import { AppUser } from '../models/AppUser';
@@ -16,12 +16,13 @@ export class AuthService {
   constructor(
     private userService: UserService,
     private afAuth: AngularFireAuth,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private routeNavigate: Router
   ) {
     this.user$ = this.afAuth.authState;
   }
 
-  login() {
+  async login() {
     // if someone hits such an url that has login access, then we should save that url in the local storage
     // after that, after server response we can send theat user to that particular page.
 
@@ -31,11 +32,25 @@ export class AuthService {
     if (returnUrl) localStorage.setItem('returnUrl', returnUrl);
     else localStorage.removeItem('returnUrl');
 
-    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    const guser = await this.afAuth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    );
+    // @ts-ignore
+    this.saveToken(guser.credential?.accessToken);
+    this.routeNavigate.navigate(['/']);
+  }
+
+  saveToken(token: any) {
+    localStorage.setItem('TOKEN', JSON.stringify(token));
+  }
+
+  isTokenAvailable() {
+    return localStorage.getItem('TOKEN') != null;
   }
 
   logout() {
     this.afAuth.signOut();
+    localStorage.removeItem('TOKEN');
   }
 
   get appUser$() {
