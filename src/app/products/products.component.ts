83655109a218b34cf61dabba7632f983c1695d26
Shuva Biswas <shuvabiswas12@gmail.com>
2022-05-29 23:31:52 +0600

    added product to home pages.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { ProductService } from './../services/product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../models/Product';
@@ -11,20 +12,37 @@ import { Product } from '../models/Product';
 })
 export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[];
+  filteredProducts: Product[];
   categories$: any;
   subscription: Subscription;
+  selectedCategory: string | null = null;
 
   constructor(
+    private router: ActivatedRoute,
     private productService: ProductService,
     private categoryService: CategoryService
   ) {
     this.products = [];
+    this.filteredProducts = [];
 
-    this.subscription = this.productService.getAll().subscribe({
-      next: (res) => {
-        if (res) this.products = res as Product[];
-      },
-    });
+    this.subscription = this.productService
+      .getAll()
+      .pipe(
+        switchMap((product) => {
+          this.products = product as Product[];
+          return this.router.queryParamMap;
+        })
+      )
+      .subscribe({
+        next: (params) => {
+          this.selectedCategory = params.get('category');
+          this.filteredProducts = this.selectedCategory
+            ? this.products.filter(
+                (p) => p.value.category === this.selectedCategory
+              )
+            : this.products;
+        },
+      });
 
     this.categories$ = this.categoryService.getCategories();
   }
